ce09a027cc3df8d95605aa1673543e6dc84823e7
yangyang0611 <https://github.com/yangyang0611>
2022-07-18 17:21:58 +0800

    [Fix]Mofidied Procfile

--- app.py
+++ app.py
@@ -415,4 +415,5 @@ def pls_come_again():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port="5000", debug=True)
+    # app.run(host="127.0.0.1", port="5000", debug=True)
+    app.run()
